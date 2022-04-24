b311a6d67ba9f74ffcdabfd8e9fe41bac6edf76c
httpmurilo <muriloeduardo1997@outlook.com>
2022-04-24 15:18:17 -0300

    Continuação

--- vendas-frontend/src/app/app.module.ts
+++ vendas-frontend/src/app/app.module.ts
@@ -3,12 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { ClientesModule} from './clientes/clientes.module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ClientesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
