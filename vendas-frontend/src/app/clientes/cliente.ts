e1267f2889b300af84dcf3d7f972111a24221474
httpmurilo <muriloeduardo1997@outlook.com>
2022-05-15 10:26:26 -0300

    Continuação do front-end

---
+++ vendas-frontend/src/app/clientes/cliente.ts
@@ -0,0 +1,5 @@
+export class Cliente {
+    id: number;
+    cpf: string;
+    dataCadastro: string;
+}
--- vendas-frontend/src/app/clientes/clientes.module.ts
+++ vendas-frontend/src/app/clientes/clientes.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ClientesComponent } from './clientes.component';
 import { ClientesFormComponent } from './clientes-form/clientes-form.component';
 import { ClientesRoutingModule } from './clientes-routing.module';
 
+import { FormsModule } from '@angular/forms'
+
+
 @NgModule({
   imports: [
     CommonModule,
-    ClientesRoutingModule
+    ClientesRoutingModule,
+    FormsModule
   ],
   declarations: [ClientesComponent, ClientesFormComponent], 
   exports: [
